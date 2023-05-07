1071e29ea8d6ad82b77213002d240270451a2d31
KarinaK <mskarinakrivko@gmail.com>
2023-05-07 22:26:32 +0200

    bucket changed

--- src/components/Bucket/index.js
+++ src/components/Bucket/index.js
@@ -14,24 +14,28 @@ function Bucket(props) {
         setBucketItems(storedBucketItems);
     };
 
+    if (bucketItems.length <= 0) {
+        return (
+            <div>
+                {<p>No items in the bucket.</p>}
+            </div>
+        );
+    }
+
     return (
         <div>
-            {bucketItems.length > 0 ? (
-                <ul>
-                    {bucketItems.map(item => (
-                        <li key={item.id}>
-                            <img src={item.image} alt={item.title} width="100" />
-                            <h3>{item.title}</h3>
-                            <p>Price: ${item.price}</p>
-                            <p>Discount Price: ${item.discont_price}</p>
-                            <p>Description: {item.description}</p>
-                            <p>Count: {item.count}</p>
-                        </li>
-                    ))}
-                </ul>
-            ) : (
-                <p>No items in the bucket.</p>
-            )}
+            {<ul>
+                {bucketItems.map(item => (
+                    <li key={item.id}>
+                        <img src={item.image} alt={item.title} width="100"/>
+                        <h3>{item.title}</h3>
+                        <p>Price: ${item.price}</p>
+                        <p>Discount Price: ${item.discont_price}</p>
+                        <p>Description: {item.description}</p>
+                        <p>Count: {item.count}</p>
+                    </li>
+                ))}
+            </ul>}
         </div>
     );
 }
