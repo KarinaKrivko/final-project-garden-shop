fa49a91615878b07e2cab16896d68939ebff4442
Karina <mskarinakrivko@gmail.com>
2023-05-17 19:47:14 +0200

    bug fixes for filter

--- src/components/Filters/index.jsx
+++ src/components/Filters/index.jsx
@@ -1,20 +1,98 @@
 import s from "./styles.module.css";
 import * as PropTypes from "prop-types";
-import React from "react";
+import React, {useEffect, useState} from "react";
+import _ from "lodash";
+import {fetchProducts, fetchProductsSuccess} from "../../actions/productsActions";
+import {useDispatch, useSelector} from "react-redux";
 
 function Filters(props) {
+
+    const dispatch = useDispatch();
+    const products = useSelector((state) => state.products.products);
+
+    const [selectedOption, setSelectedOption] = useState("");
+    const [sortedProducts, setSortedProducts] = useState([]);
+    const [filteredProducts, setFilteredProducts] = useState([]);
+    const [discountProducts, setDiscountProducts] = useState([]);
+    const [originalProducts, setOriginalProducts] = useState([]);
+
+    const handleOptionChange = (event) => {
+        setSelectedOption(event.target.value);
+        if (event.target.value === "price-low-high") {
+            const sortedProducts = [...products].sort((a, b) => a.price - b.price);
+            setSortedProducts(sortedProducts);
+        } else {
+            if (event.target.value === "price-high-low") {
+                const sortedProducts = [...products].sort((a, b) => b.price - a.price);
+                setSortedProducts(sortedProducts);
+            }
+        }
+    };
+
+
+    const handleFilterChange = (event) => {
+        if (event.keyCode!==13) return;
+
+        const priceFrom = document.getElementById('priceLow').value;
+        const priceTo = document.getElementById('priceHigh').value;
+
+        let low = _.toInteger(priceFrom);
+        let high = _.toInteger(priceTo);
+
+        if (high === 0) return
+        const filteredProducts = originalProducts
+            .filter((it) => it.price <= high)
+            .filter((it) => it.price > low)
+
+        setFilteredProducts(filteredProducts)
+    }
+
+    const handleDiscountCheckBox = (event) => {
+        setSelectedOption(event.target.value);
+        const discountProducts = products.filter((el) => el.discont_price != null);
+        if (event.target.checked) {
+            setDiscountProducts(discountProducts);
+            setOriginalProducts(products);
+            dispatch(fetchProductsSuccess(discountProducts));
+        } else {
+            setSelectedOption("");
+            setDiscountProducts(originalProducts);
+            dispatch(fetchProductsSuccess(originalProducts));
+        }
+    };
+
+    useEffect(() => {
+        setOriginalProducts(products);
+    }, [products]);
+
+    useEffect(() => {
+        dispatch(fetchProducts());
+    }, [dispatch]);
+
+    useEffect(() => {
+        dispatch(fetchProductsSuccess(sortedProducts));
+    }, [sortedProducts, dispatch]);
+
+    useEffect(() => {
+        dispatch(fetchProductsSuccess(discountProducts));
+    }, [discountProducts, dispatch]);
+
+    useEffect(() => {
+        dispatch(fetchProductsSuccess(filteredProducts));
+    }, [filteredProducts, dispatch]);
+
     return <div>
         <div className={s.tools_container}>
             <div className={s.price}>Price</div>
-            <input id="priceLow" className={s.from} onBlur={props.onBlur} placeholder="from"></input>
-            <input id="priceHigh" className={s.to} onBlur={props.onBlur} placeholder="to"></input>
+            <input id="priceLow" className={s.from} onKeyDown={handleFilterChange} onBlur={handleFilterChange} placeholder="from"></input>
+            <input id="priceHigh" className={s.to} onKeyDown={handleFilterChange} onBlur={handleFilterChange} placeholder="to"></input>
 
             {props.showDiscountCmp===true &&
                 <>
                     <div className={s.discounted}>Discounted items</div>
                     <input id="discountCheckBox"
                            type={"checkbox"}
-                           onChange={props.onChangeCheckbox}
+                           onChange={handleDiscountCheckBox}
                            className={s.inputForDiscounted}
                            data-testid="discount-checkbox"
                     />
@@ -27,7 +105,7 @@ function Filters(props) {
                 className={s.default}
                 id="my-select"
                 value={props.value}
-                onChange={props.onChangeSelect}
+                onChange={handleOptionChange}
                 data-testid="sort-select"
             >
                 <option value="by default">by default</option>
@@ -39,10 +117,7 @@ function Filters(props) {
 }
 
 Filters.propTypes = {
-    onBlur: PropTypes.func,
-    onChangeCheckbox: PropTypes.func,
     value: PropTypes.string,
-    onChangeSelect: PropTypes.func,
     showDiscountCmp: PropTypes.bool
 };
 
--- src/pages/AllProductsPage/index.jsx
+++ src/pages/AllProductsPage/index.jsx
@@ -8,84 +8,18 @@ import Filters from "../../components/Filters";
 
 
 function AllProductsPage() {
+
     const dispatch = useDispatch();
     const products = useSelector((state) => state.products.products);
-
-    const [selectedOption, setSelectedOption] = useState("");
-    const [sortedProducts, setSortedProducts] = useState([]);
-    const [filteredProducts, setFilteredProducts] = useState([]);
-    const [discountProducts, setDiscountProducts] = useState([]);
-    const [originalProducts, setOriginalProducts] = useState([]);
-
-    const handleOptionChange = (event) => {
-        setSelectedOption(event.target.value);
-        if (event.target.value === "price-low-high") {
-            const sortedProducts = [...products].sort((a, b) => a.price - b.price);
-            setSortedProducts(sortedProducts);
-        } else {
-            if (event.target.value === "price-high-low") {
-                const sortedProducts = [...products].sort((a, b) => b.price - a.price);
-                setSortedProducts(sortedProducts);
-            }
-        }
-    };
-
-
-    const handleFilterChange = (event) => {
-        const priceFrom = document.getElementById('priceLow').value;
-        const priceTo = document.getElementById('priceHigh').value;
-
-        let low = _.toInteger(priceFrom);
-        let high = _.toInteger(priceTo);
-
-        if (high === 0) return
-        const filteredProducts = originalProducts
-            .filter((it) => it.price <= high)
-            .filter((it) => it.price > low)
-
-        setFilteredProducts(filteredProducts)
-    }
-
-    const handleDiscountCheckBox = (event) => {
-        setSelectedOption(event.target.value);
-        const discountProducts = products.filter((el) => el.discont_price != null);
-        if (event.target.checked) {
-            setDiscountProducts(discountProducts);
-            setOriginalProducts(products);
-            dispatch(fetchProductsSuccess(discountProducts));
-        } else {
-            setSelectedOption("");
-            setDiscountProducts(originalProducts);
-            dispatch(fetchProductsSuccess(originalProducts));
-        }
-    };
-
-    useEffect(() => {
-        setOriginalProducts(products);
-    }, [products]);
 
     useEffect(() => {
         dispatch(fetchProducts());
     }, [dispatch]);
 
-    useEffect(() => {
-        dispatch(fetchProductsSuccess(sortedProducts));
-    }, [sortedProducts, dispatch]);
-
-    useEffect(() => {
-        dispatch(fetchProductsSuccess(discountProducts));
-    }, [discountProducts, dispatch]);
-
-    useEffect(() => {
-        dispatch(fetchProductsSuccess(filteredProducts));
-    }, [filteredProducts, dispatch]);
-
-
     return (
         <div>
             <h1 className={s.pageName}>All products</h1>
-            <Filters showDiscountCmp={true} onBlur={handleFilterChange} onChangeCheckbox={handleDiscountCheckBox} value={selectedOption}
-                     onChangeSelect={handleOptionChange}/>
+            <Filters showDiscountCmp={true} />
             <div className={s.productContainer}>
                 {products.map((product) => (
                     <ProductItem key={product.id} product={product}/>
